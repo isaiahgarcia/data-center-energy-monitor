e5b54fa0d94f076f6e58e37e1859ca4495f891a2
isaiahgarcia <isaiahgarcia@umail.ucsb.edu>
2024-02-08 15:02:49 -0800

    ig - header partially done and main page content done

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import Image from "next/image";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const Header = () => {
+    return (
+        <div className={`${inter.className} bg-primary h-[70px]`}>
+            <div className="flex flex-row justify-between">
+                <div className="flex flex-row items-center space-x-5">
+                    <Image
+                        src="/images/isalab-logo.svg"
+                        width={200}
+                        height={200}
+                        alt="Image"
+                    />
+                    <h1 className="text-lg">
+                        Data Center Energy Monitor
+                    </h1>
+                </div>
+                <div className="flex flex-row space-x-2">
+                    <p>Navbar</p>
+                    <p>Button</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
